f572b831cb3345fcca1d9e77ddff6970b652004a
Bogdan Bosca <bogdan.bucsa@travelport.com>
2019-03-19 09:40:26 +0200

    fetch example

--- es6/src/index.js
+++ es6/src/index.js
@@ -1,28 +1,37 @@
-import MyPromise from './MyPromise';
+// import MyPromise from './MyPromise';
+//
+// const promiseTest = () => {
+//     const foo = () => new MyPromise((r) => r('test'));
+//     foo().then((p) => {
+//         throw new Error(`error in then ${p}`);
+//     }).catch(console.log);
+//
+//     const bar = () => new MyPromise((r, reject) => reject('test'));
+//     bar().catch(console.log);
+//
+//     const fooBar = () => new Promise(r => r({a: 'obj test'}));
+//     fooBar().then(obj => obj.a).then(console.log);
+//
+//     MyPromise.test([1, 2, 3, 4]);
+// };
+//
+// const asyncTest = async () => {
+//     const foo = async () => {
+//         return "async test";
+//     };
+//
+//     console.log(await foo());
+// };
+//
+// // promiseTest();
+// // asyncTest();
+//
 
-const promiseTest = () => {
-    const foo = () => new MyPromise((r) => r('test'));
-    foo().then((p) => {
-        throw new Error(`error in then ${p}`);
-    }).catch(console.log);
+import fetch from 'node-fetch';
 
-    const bar = () => new MyPromise((r, reject) => reject('test'));
-    bar().catch(console.log);
-
-    const fooBar = () => new Promise(r => r({a: 'obj test'}));
-    fooBar().then(obj => obj.a).then(console.log);
-
-    MyPromise.test([1, 2, 3, 4]);
-};
-
-const asyncTest = async () => {
-    const foo = async () => {
-        return "async test";
-    };
-
-    console.log(await foo());
-};
-
-// promiseTest();
-// asyncTest();
-
+const body = {text: 'test'};
+fetch('http://localhost:3000/note', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(body),
+}).then(r => r.json()).then(console.log).catch(e => console.log(e));
